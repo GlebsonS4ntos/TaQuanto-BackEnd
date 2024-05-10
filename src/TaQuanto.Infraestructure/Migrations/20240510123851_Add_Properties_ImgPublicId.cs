04c6e6e341a316db02ac52929eeb3843f234caa4
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-10 11:06:12 -0300

    Add Proprety ImgPublicId

---
+++ src/TaQuanto.Infraestructure/Migrations/20240510123851_Add_Properties_ImgPublicId.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaQuanto.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Properties_ImgPublicId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePublicId",
+                table: "Product",
+                type: "varchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePublicId",
+                table: "Establishment",
+                type: "varchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePublicId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePublicId",
+                table: "Establishment");
+        }
+    }
+}
