171c302f21c0be6f635fdc02a7fb37eda5d92b10
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-10 11:08:11 -0300

    Service AddPhoto to Cloudinary

---
+++ src/TaQuanto.Service/Services/ServicePhoto.cs
@@ -0,0 +1,43 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using CloudinaryDotNet.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using TaQuanto.Service.Helpers;
+using TaQuanto.Service.Interfaces;
+
+namespace TaQuanto.Service.Services
+{
+    public class ServicePhoto : IServicePhoto
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public ServicePhoto(IOptions<CloudinaryConfig> cloudinaryConfig)
+        {
+            var acc = new Account
+            (
+                cloudinaryConfig.Value.CloudName,
+                cloudinaryConfig.Value.ApiKey,
+                cloudinaryConfig.Value.ApiSecret
+            );
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddPhoto(IFormFile img)
+        {
+            var stream = img.OpenReadStream();
+            var uploadOpt = new ImageUploadParams()
+            {
+                File = new FileDescription(new Guid().ToString(), stream)
+            };
+            return await _cloudinary.UploadAsync(uploadOpt);
+        }
+
+        public async Task<DeletionResult> DeletePhoto(string imgUrl)
+        {
+            var deleteParameters = new DeletionParams(imgUrl);
+            var result = await _cloudinary.DestroyAsync(deleteParameters);
+            return result;
+        }
+    }
+}
