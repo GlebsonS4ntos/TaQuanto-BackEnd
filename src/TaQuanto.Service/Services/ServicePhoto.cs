c8ae84eae1427071a21f3d7d10504b97f46e74b2
Glebson Santos <glebsonsantos@outlook.pt>
2024-05-18 00:26:55 -0300

    Transform Image in .webp

--- src/TaQuanto.Service/Services/ServicePhoto.cs
+++ src/TaQuanto.Service/Services/ServicePhoto.cs
@@ -1,8 +1,8 @@
 ﻿using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
-using CloudinaryDotNet.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using SixLabors.ImageSharp;
 using TaQuanto.Service.Helpers;
 using TaQuanto.Service.Interfaces;
 
@@ -25,12 +25,32 @@ namespace TaQuanto.Service.Services
 
         public async Task<ImageUploadResult> AddPhoto(IFormFile img)
         {
-            var stream = img.OpenReadStream();
-            var uploadOpt = new ImageUploadParams()
+            var webpBytes = ConvertToWebP(img);
+
+            using (var stream = new MemoryStream(webpBytes))
             {
-                File = new FileDescription(new Guid().ToString(), stream)
-            };
-            return await _cloudinary.UploadAsync(uploadOpt);
+                var uploadOpt = new ImageUploadParams()
+                {
+                    File = new FileDescription(new Guid().ToString(), stream)
+                };
+                return await _cloudinary.UploadAsync(uploadOpt);
+            }
+        }
+
+        private byte[] ConvertToWebP(IFormFile img)
+        {
+            using (var inputStream = img.OpenReadStream())
+            {
+                using (var image = Image.Load(inputStream))
+                {
+                    var encoder = new SixLabors.ImageSharp.Formats.Webp.WebpEncoder { Quality = 75 }; // Ajuste a qualidade conforme necessário
+                    using (var outputStream = new MemoryStream())
+                    {
+                        image.Save(outputStream, encoder);
+                        return outputStream.ToArray();
+                    }
+                }
+            }
         }
 
         public async Task<DeletionResult> DeletePhoto(string imgUrl)
